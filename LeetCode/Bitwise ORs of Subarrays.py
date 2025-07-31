06006f5ad50c1b00f6552ac70c0d8155630f2484
Paolo Espiritu <paolo.v.espiritu@gmail.com>
2025-07-31 18:48:36 +0800

    chore(update): #898 library

--- LeetCode/Bitwise ORs of Subarrays.py
+++ LeetCode/Bitwise ORs of Subarrays.py
@@ -1,10 +1,13 @@
+from typing import List
+
+
 class Solution:
     def subarrayBitwiseORs(self, arr: List[int]) -> int:
         ans, cur = set(), set()
 
         for i in arr:
-            cur = {i | j for j in cur} # get the bitwise OR of i and j
-            cur.add(i) # add the next number for the next iteration
-            ans |= cur # update ans dictionary
+            cur = {i | j for j in cur}  # get the bitwise OR of i and j
+            cur.add(i)  # add the next number for the next iteration
+            ans |= cur  # update ans dictionary
 
         return len(ans)
